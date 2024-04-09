cabc851852bf95cf6461aab23b428d24566bc612
farhan <el3317.farhanmahmood@gmail.com>
2024-04-10 01:28:11 +0530

    comparision and logical operator

--- src/app.js
+++ src/app.js
@@ -5,32 +5,72 @@ mongoose
   .then(() => console.log("connection successful....."))
   .catch((err) => console.log(err));
 
-//schema-camelCase
+// schema-camelCase
 const playlistSchema = new mongoose.Schema({
   name: String,
   type: String,
   Videos: Number,
-  author: String,
   active: Boolean,
   date: {
     type: Date,
     defult: Date.now,
   },
 });
-//models or collection creation-Pascalcase
+// models or collection creation-Pascalcase
 const Playlist = new mongoose.model("Playlist", playlistSchema);
-const createDocument = async () => {
+// const createDocument = async () => {
+//   try {
+//     const javaPlaylist = new Playlist({
+//       name: "Java",
+//       type: "Front End",
+//       Videos: 69,
+//       active: true,
+//     });
+//     const javascriptPlaylist = new Playlist({
+//       name: "JavaScript",
+//       type: "Front End",
+//       Videos: 99,
+//       active: false,
+//     });
+//     const nodePlaylist = new Playlist({
+//       name: "NodeJS",
+//       type: "Back End",
+//       Videos: 29,
+//       active: true,
+//     });
+//     const SQLPlaylist = new Playlist({
+//       name: "SQL",
+//       type: "Database",
+//       Videos: 59,
+//       active: true,
+//     });
+//     const result = await Playlist.insertMany([
+//       javaPlaylist,
+//       javascriptPlaylist,
+//       nodePlaylist,
+//       SQLPlaylist,
+//     ]);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// createDocument();
+
+//read operation
+const getData = async () => {
   try {
-    const reactPlaylist = new Playlist({
-      name: "React",
-      type: "Front End",
-      Videos: 49,
-      active: true,
-    });
-    const result = await reactPlaylist.save();
+    const result = await Playlist.find({
+      $and: [{ type: "Front End" }, { active: true }],
+    })
+      // .select({
+      //   name: 1,
+      // })
+      .countDocuments();
     console.log(result);
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 };
-createDocument();
+
+getData();
